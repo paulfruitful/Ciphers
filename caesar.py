5115eff433bab1125c0358c98abe0e46a02d7b0a
Paul Fruitful <fruitful2007@outlook.com>
2024-05-22 01:27:12 +0100

    Caesar Cipher decrypt

--- caesar.py
+++ caesar.py
@@ -11,8 +11,27 @@ class CaesarCipher:
     def encrypt(self):
         for i in self.text:
             curr=self.symbols.find(i)
-            curr=self.key-len(self.symbols)
+            curr=(self.key+curr)-len(self.symbols)
             self.cipher+=self.symbols[curr]
+        return print(self.cipher)
+
+    def decrypt(self):
+        for i in self.cipher:
+            curr=self.symbols.find(i)
+            curr=(curr-self.key)-len(self.symbols)
+            self.cipher+=self.symbols[curr]
+        return print(self.cipher)
+    
+    def change_text(self,text):
+        self.text=text
+
+
+caat=CaesarCipher(3,'Cat')
+caat.encrypt()
+caat.decrypt()
+
+
+
 
 
         
