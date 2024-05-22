7d9d4ed01075020536697ad55a26cbe83b2a69d8
Paul Fruitful <fruitful2007@outlook.com>
2024-05-22 02:57:07 +0100

    successful decrypt

--- caesar.py
+++ caesar.py
@@ -1,33 +1,38 @@
 
 class CaesarCipher:
-    def __init__(self,key,text) :
+    def __init__(self,key) :
         
-        self.symbols='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+={}[]|\:;\"\',<>./?Δƒ€ŠŒ©®§'
-        if key<len(self.symbols):
+        self.symbols='abcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()_-+={}[]|\:;\"\',<>./?Δƒ€ŠŒ©®§'
+        self.char_dict=  {char: index for index, char in enumerate(self.symbols)}
+        if key>len(self.symbols):
             return print('Key can\'t be greater than 26 ')
         self.key=key
-        self.text=text
+        self.text=''
         self.cipher=''
-    def encrypt(self):
-        for i in self.text:
-            curr=self.symbols.find(i)
-            curr=(self.key+curr)-len(self.symbols)
+    def encrypt(self,text):
+        text=text.lower()
+        for i in text:
+            curr=self.char_dict[i]
+            
+            curr=(self.key+curr)%len(self.symbols) 
+           
             self.cipher+=self.symbols[curr]
         return print(self.cipher)
 
     def decrypt(self):
         for i in self.cipher:
-            curr=self.symbols.find(i)
-            curr=(curr-self.key)-len(self.symbols)
-            self.cipher+=self.symbols[curr]
-        return print(self.cipher)
+            curr=self.char_dict[i]
+           
+            curr=(curr-self.key)%len(self.symbols)
+           
+            self.text+=self.symbols[curr]
+      
+        return print(self.text)
     
-    def change_text(self,text):
-        self.text=text
 
 
-caat=CaesarCipher(3,'Cat')
-caat.encrypt()
+caat=CaesarCipher(74)
+caat.encrypt('amk')
 caat.decrypt()
 
 
