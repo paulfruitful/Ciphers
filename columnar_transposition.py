6d3a544f4b03f1aabcc19c2941f133d6a314ad08
Paul Fruitful <fruitful2007@outlook.com>
2024-05-24 05:24:09 +0100

    ........

--- columnar_transposition.py
+++ columnar_transposition.py
@@ -12,20 +12,31 @@ class ColumnarCipher:
                 currColumn+=key
         print(''.join(columns))
         return ''.join(columns)
-    def decrypt(self,key,text):
-        columns=['']*key
-        for i in range(key):
-            currColumn=i
-            while currColumn<len(text):
-                columns[i]+=text[currColumn]
-                currColumn+=key
-        return print(''.join(columns))
-    
+    def decrypt(self, key, cipher):
+        if key < 2 or key >= len(cipher):
+            return 'Your Key Is Not Strong Enough'
+
+        numOfColumns = len(cipher) // key
+        numOfRows = key
+        numOfShadedBoxes = (numOfColumns * numOfRows) - len(cipher)
+
+        plaintext = [''] * numOfColumns
+        column = 0
+        row = 0
+        for symbol in cipher:
+         plaintext[column] += symbol
+         column += 1
+
+         if (column == numOfColumns) or (column == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
+            column = 0
+            row += 1
+
+        return print(''.join(plaintext))
     
     
 test=ColumnarCipher()
-x=test.encrypt(8,'I got charito my side dish and she couldn\'t ask for more lol ')
-test.decrypt(8,x)
+x=test.encrypt(3,'I got charito my side dish and she couldn\'t ask for more lol ')
+test.decrypt(3,x)
 
     
         
