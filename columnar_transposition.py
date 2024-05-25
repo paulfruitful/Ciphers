c100888c4bb1750fba8f35789629208cbdfd7e32
Paul Fruitful <fruitful2007@outlook.com>
2024-05-25 11:11:46 +0100

    ..........

--- columnar_transposition.py
+++ columnar_transposition.py
@@ -1,3 +1,4 @@
+import math
 class ColumnarCipher:
     def __init__(self):
         self.cipher=''
@@ -16,26 +17,27 @@ class ColumnarCipher:
         if key < 2 or key >= len(cipher):
             return 'Your Key Is Not Strong Enough'
 
-        numOfColumns = len(cipher) // key
-        numOfRows = key
-        numOfShadedBoxes = (numOfColumns * numOfRows) - len(cipher)
-
-        plaintext = [''] * numOfColumns
-        column = 0
-        row = 0
-        for symbol in cipher:
-         plaintext[column] += symbol
-         column += 1
-
-         if (column == numOfColumns) or (column == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
-            column = 0
-            row += 1
-
+        numOfrows = int(round(float(len(cipher)) / float(key), 0))  # Convert the rounded result to an integer
+        numOfcols = key
+        plaintext = [''] * numOfrows
+        col=0
+        row=0
+        for i in range(len(cipher)):
+         currCol=col
+         while currCol<len(cipher) :
+           plaintext[row] += cipher[currCol]  # Append the character to the current row
+           print(currCol,cipher[currCol])
+           currCol+=numOfrows
+         
+         col += 1
+      
+        print(len(cipher))
+        print(len(''.join(plaintext)))
         return print(''.join(plaintext))
-    
+   
     
 test=ColumnarCipher()
-x=test.encrypt(3,'I got charito my side dish and she couldn\'t ask for more lol ')
+x=test.encrypt(3,'Igotcharito mysidedishandshe couldnt a skformorelol ')
 test.decrypt(3,x)
 
     
