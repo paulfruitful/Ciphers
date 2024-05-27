cf1c9bd4f8e49fc290c1413806ac4f7a5b09898d
Paul Fruitful <fruitful2007@outlook.com>
2024-05-28 00:28:50 +0100

    the decrypt fix for columnar

--- columnar_transposition.py
+++ columnar_transposition.py
@@ -2,37 +2,39 @@ import math
 class ColumnarCipher:
   def __init__(self):
         self.cipher=''
+  
   def encrypt(self,key, text):
-     if key < 2 or key >= len(text):
+    if key < 2 or key >= len(text):
         return 'Your Key Is Not Strong Enough'
     
-     columns = [''] * key
+    columns = [''] * key
     
-     for i in range(key):
+    for i in range(key):
         currColumn = i
         while currColumn < len(text):
             columns[i] += text[currColumn]
             currColumn += key
+    ciphertext = ''.join(columns)
     
-     return ''.join(columns)
+    return ciphertext
 
   def decrypt(self,key, ciphertext):
+    if key < 2 or key >= len(ciphertext):
+        return 'Your Key Is Not Strong Enough'
     
-    
-    num_full_rows = len(ciphertext) // key
+    num_full_columns = len(ciphertext) // key
     num_extra_chars = len(ciphertext) % key
-    
     
     columns = [''] * key
     
     index = 0
     
     for i in range(key):
-        num_chars_in_column = num_full_rows + (1 if i < num_extra_chars else 0)
+        num_chars_in_column = num_full_columns + (1 if i < num_extra_chars else 0)
         columns[i] = ciphertext[index:index + num_chars_in_column]
         index += num_chars_in_column
     plaintext = []
-    for i in range(num_full_rows + 1):
+    for i in range(num_full_columns + (1 if num_extra_chars > 0 else 0)):
         for col in columns:
             if i < len(col):
                 plaintext.append(col[i])
@@ -44,9 +46,10 @@ class ColumnarCipher:
    
     
 test=ColumnarCipher()
-x=test.encrypt(3,'catholic my man eats hat bick please helperand make sheep so kay')
+x=test.encrypt(10,'catholic my man eats hat bick please helperand make sheep so kay')
+print(x)
 #test.decrypt(3,x)
-plaintext = test.decrypt(3, x)
+plaintext = test.decrypt(10, x)
 print(plaintext)
 
     
