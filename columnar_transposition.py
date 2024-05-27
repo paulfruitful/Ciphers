2025a40541d57f04649ce3859a03fea2d5c9485a
Paul Fruitful <fruitful2007@outlook.com>
2024-05-27 03:27:02 +0100

    the decrypt fix for columnar

--- columnar_transposition.py
+++ columnar_transposition.py
@@ -1,44 +1,53 @@
 import math
 class ColumnarCipher:
-    def __init__(self):
+  def __init__(self):
         self.cipher=''
-    def encrypt(self,key,text):
-        if key<2 or key>=len(text):
-            return print('Your Key Is Not Strong Enough')
-        columns=['']*key
-        for i in range(key):
-            currColumn=i
-            while currColumn<len(text):
-                columns[i]+=text[currColumn]
-                currColumn+=key
-        print(''.join(columns))
-        return ''.join(columns)
-    def decrypt(self, key, cipher):
-        if key < 2 or key >= len(cipher):
-            return 'Your Key Is Not Strong Enough'
+  def encrypt(self,key, text):
+     if key < 2 or key >= len(text):
+        return 'Your Key Is Not Strong Enough'
+    
+     columns = [''] * key
+    
+     for i in range(key):
+        currColumn = i
+        while currColumn < len(text):
+            columns[i] += text[currColumn]
+            currColumn += key
+    
+     return ''.join(columns)
 
-        numOfrows = int(round(float(len(cipher)) / float(key), 0))  # Convert the rounded result to an integer
-        numOfcols = key
-        plaintext = [''] * numOfrows
-        col=0
-        row=0
-        for i in range(len(cipher)):
-         currCol=col
-         while currCol<len(cipher) :
-           plaintext[row] += cipher[currCol]  # Append the character to the current row
-           print(currCol,cipher[currCol])
-           currCol+=numOfrows
-         
-         col += 1
-      
-        print(len(cipher))
-        print(len(''.join(plaintext)))
-        return print(''.join(plaintext))
+  def decrypt(self,key, ciphertext):
+    
+    
+    num_full_rows = len(ciphertext) // key
+    num_extra_chars = len(ciphertext) % key
+    
+    
+    columns = [''] * key
+    
+    index = 0
+    
+    for i in range(key):
+        num_chars_in_column = num_full_rows + (1 if i < num_extra_chars else 0)
+        columns[i] = ciphertext[index:index + num_chars_in_column]
+        index += num_chars_in_column
+    plaintext = []
+    for i in range(num_full_rows + 1):
+        for col in columns:
+            if i < len(col):
+                plaintext.append(col[i])
+    
+    return ''.join(plaintext)
+
+
+
    
     
 test=ColumnarCipher()
-x=test.encrypt(3,'Igotcharito mysidedishandshe couldnt a skformorelol ')
-test.decrypt(3,x)
+x=test.encrypt(3,'catholic my man eats hat bick please helperand make sheep so kay')
+#test.decrypt(3,x)
+plaintext = test.decrypt(3, x)
+print(plaintext)
 
     
         
